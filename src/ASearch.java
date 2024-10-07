c32effe083c7cf50d2f274d3c159e68402338eaf
goncalo-122 <132440667+goncalo-122@users.noreply.github.com>
2024-10-07 08:32:09 +0100

    Version2 Class Puzzle 8 AStar

--- src/ASearch.java
+++ src/ASearch.java
@@ -1,2 +1,48 @@
+import javax.swing.plaf.nimbus.State;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class ASearch {
+
+    public static Node aStarSearch(Puzzle8 inicialState){
+        //Initialize the fringe with teh inicial state
+        Fringe fringe=new Fringe(new Node(inicialState,null,0));
+        //Set-safe unic value once
+        //hashSet-efficiency table to garantee that arent duplications.
+        Set<IState> closedSet = new HashSet<>();
+        while(true) {
+            if (fringe.isEmpty()) {
+                return null;
+            } else {
+                //remove the next node and meets the state of him
+                Node currentNode = fringe.getFringeNode();
+                IState currentState = currentNode.getState();
+                if (currentState.goal()) {
+                    return currentNode;
+                }
+
+                // Else-Close state to guarantee that isn t used again
+                closedSet.add(currentState);
+                //Create ArrayList with next possible Actions
+                ArrayList<Action> sucessors = currentState.suc();
+                for (Action action : sucessors) {
+                    //Create a new Node
+                    Node newNode = new Node(action.getState(), currentNode, action.cost);
+                    //if action of the newNode isn t in the closedState add it to the fringeNode
+                    if (!closedSet.contains(newNode.getState())) {
+                        fringe.addFringeNode(newNode);
+                    }
+                }
+
+            }
+
+
+            return null;// doesnt find the next action/state that takes it to the next state
+        }
+    }
+
+
 }
+     
--- src/Puzzle8.java
+++ src/Puzzle8.java
@@ -1,4 +1,169 @@
-public class Puzzle8 {
+import java.util.ArrayList;
 
+public class Puzzle8 implements IState {
+    int[][] state;
+    int zeroRow, zeroCol; // Position of the empty tile (0)
+
+    public Puzzle8(int[][] state) {
+        this.state = state;
+        getEmptyTile();
+    }
+
+    // function to find empty tile
+    public void getEmptyTile(){
+        for (int i = 0; i < state.length; i++) {
+            for (int j = 0; j < state[i].length; j++) {
+                if (state[i][j] == 0) {
+                    zeroRow = i;
+                    zeroCol = j;
+                    return;
+                }
+            }
+        }
+    }
+    //Function moves 1 line Up empty tile. Update zeroRow->zeroRow-- and update in matrix empty tile position->swap(zeroRow,zeroCol,zeroRow-1,zeroCol);
+    public  void moveUp(){
+        if(zeroRow>0){
+            swap(zeroRow,zeroCol,zeroRow-1,zeroCol);
+            zeroRow--;
+        }
+    }
+
+    //Function moves 1 line down empty tile. Verify if has more row after zerorow and then swap it
+    public void moveDown(){
+        if (zeroRow< state.length-1){
+            swap(zeroRow,zeroCol,zeroRow+1,zeroCol);
+            zeroRow++;
+        }
+    }
+    //Function moves 1 colum left if it is not the first one.
+    public void moveLeft(){
+        if(zeroCol>0){
+            swap(zeroRow,zeroCol,zeroRow,zeroCol-1);
+            zeroCol--;
+        }
+    }
+
+    //Function moves 1 colum right empty tile. Verify if has more colum after zerorow and then swap it
+    public void moveRight(){
+        if (zeroCol< state[0].length-1){
+            swap(zeroRow,zeroCol,zeroRow,zeroCol+1);
+            zeroCol++;
+        }
+    }
+    //swap location of empty tile
+    public void swap( int row1,int colum1,int row2,int colum2){
+        int temp=state[row1][colum1];
+        state[row1][colum1]=state[row2][colum2];
+        state[row2][colum2]=state[row1][colum1];
+    }
+
+
+
+//Implemented methods of Istate
+
+    @Override
+    //h represents number of tiles that are out of place
+    public double h() {
+        int[][] goalState = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 0}
+        };
+
+        int misplacedTiles = 0;
+        //comparasion between goal state and actual state to count down tiles of place
+        for (int line = 0; line < state.length; line++) {
+            for (int colum = 0; colum < state[line].length; colum++) {
+                if (state[line][colum] != goalState[line][colum]) {
+                    misplacedTiles++;
+                }
+            }
+        }
+        return misplacedTiles;
+    }
+
+    @Override
+    //Verify if any actual state is on the final position
+    public boolean goal() {
+        int[][] goalState = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 0}
+        };
+
+        for (int i = 0; i < state.length; i++) {
+            for (int j = 0; j < state[i].length; j++) {
+                if (state[i][j] != goalState[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
+
+    // this method looks to  sucessors states from the actual state. Returns actions that save sucessors states
+    @Override
+    public ArrayList<Action> suc() {
+        ArrayList<Action> actions = new ArrayList<>();
+
+
+        if (zeroRow > 0) {
+            Puzzle8 newState = new Puzzle8(cloneState());
+            newState.moveUp();
+            actions.add(new Action(newState, 1));
+        }
+
+        if (zeroRow < state.length - 1) {
+            Puzzle8 newState = new Puzzle8(cloneState());
+            newState.moveDown();
+            actions.add(new Action(newState, -1));
+        }
+
+
+        if (zeroCol > 0) {
+            Puzzle8 newState = new Puzzle8(cloneState());
+            newState.moveLeft();
+            actions.add(new Action(newState, 1));
+        }
+
+
+        if (zeroCol < state[0].length - 1) {
+            Puzzle8 newState = new Puzzle8(cloneState());
+            newState.moveRight();
+            actions.add(new Action(newState, -1));
+        }
+
+        return actions;
+    }
+
+    //Clones all states of matrix to facilitate the search of next state
+    private int[][] cloneState() {
+        int[][] newState = new int[state.length][state[0].length];
+        for (int i = 0; i < state.length; i++) {
+            System.arraycopy(state[i], 0, newState[i], 0, state[i].length);
+        }
+        return newState;
+    }
+
+
+
+    public String toString(){
+        String result="";
+        for (int row = 0; row < state.length; row++) {
+            for (int colum = 0; colum < state[row].length; colum++) {
+                result+=state[row][colum]+ " ";
+
+            }
+            result+="\n";
+        }
+        return result;
+
+
+    }
 
 }
+
